f4f583c5c73e84aa57be15431c022685d92f73f5
Haruite <89329576+Haruite@users.noreply.github.com>
2025-03-16 18:25:38 +0800

    Update BluraySubtitle.py

--- BluraySubtitle.py
+++ BluraySubtitle.py
@@ -10,7 +10,7 @@ import traceback
 from dataclasses import dataclass
 from functools import reduce, partial
 from struct import unpack
-from typing import Optional
+from typing import Optional, Dict, Union
 
 from PyQt6.QtCore import QCoreApplication
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QFileDialog, QLabel, QToolButton, QLineEdit, \
@@ -492,7 +492,9 @@ class BluraySubtitle:
                     selected_mpls = os.path.join(bluray_folder, 'BDMV', 'PLAYLIST', mpls_file)
                     yield bluray_folder, Chapter(selected_mpls), selected_mpls[:-5]
 
-    def generate_configuration(self, table: QTableWidget):
+    def generate_configuration(self, table: QTableWidget,
+                               subtitle_index: Optional[int]=None,
+                               chapter_index: Optional[int]=None) -> Dict[int, Dict[str, Union[int, str]]]:
         configuration = {}
         sub_index = 0
         subtitle_files = [os.path.join(self.input_path, path) for path in os.listdir(self.input_path)
@@ -506,17 +508,21 @@ class BluraySubtitle:
             configuration[sub_index] = {'folder': folder, 'selected_mpls': selected_mpls,
                                         'bdmv_index': bdmv_index, 'chapter_index': 1, 'offset': '0'}
             j = 1
+            fix_chapter = False
             for i, play_item_in_out_time in enumerate(chapter.in_out_time):
                 play_item_marks = chapter.mark_info.get(i)
                 chapter_num = len(play_item_marks or [])
                 if play_item_marks:
                     play_item_duration_time = play_item_in_out_time[2] - play_item_in_out_time[1]
-
                     time_shift = (start_time + play_item_marks[0] - play_item_in_out_time[1]) / 45000
                     if time_shift > sub_file.max_end_time() - 300:
                         if (sub_index + 1 < len(subtitle_files)
                                 and left_time > Subtitle(subtitle_files[sub_index + 1]).max_end_time() - 180):
                             sub_index += 1
+                            if sub_index == subtitle_index:
+                                if chapter_index > j:
+                                    fix_chapter = True
+                                    break
                             sub_file.append_ass(subtitle_files[sub_index], time_shift)
                             configuration[sub_index] = {'folder': folder, 'selected_mpls': selected_mpls,
                                 'bdmv_index': bdmv_index, 'chapter_index': j, 'offset': get_time_str(time_shift)}
@@ -536,7 +542,32 @@ class BluraySubtitle:
                 start_time += play_item_in_out_time[2] - play_item_in_out_time[1]
                 left_time += (play_item_in_out_time[1] - play_item_in_out_time[2]) / 45000
 
-            sub_index += 1
+            if fix_chapter:
+                j = 0
+                time_shift = 0
+                for i, play_item_in_out_time in enumerate(chapter.in_out_time):
+                    play_item_marks = chapter.mark_info.get(i)
+                    if (sub_index == subtitle_index and j == chapter_index
+                        or sub_index != subtitle_index and time_shift > sub_file.max_end_time()):
+                        sub_file.append_ass(subtitle_files[sub_index], time_shift)
+                        configuration[sub_index] = {'folder': folder, 'selected_mpls': selected_mpls,
+                                                    'bdmv_index': bdmv_index, 'chapter_index': j,
+                                                    'offset': get_time_str(time_shift)}
+                        sub_index += 1
+                    if play_item_marks:
+                        for mark in play_item_marks:
+                            j += 1
+                            shift = time_shift + (mark - play_item_in_out_time[1]) / 45000
+                            if (sub_index == subtitle_index and j == chapter_index
+                                or sub_index != subtitle_index and shift > sub_file.max_end_time()):
+                                sub_file.append_ass(subtitle_files[sub_index], shift)
+                                configuration[sub_index] = {'folder': folder, 'selected_mpls': selected_mpls,
+                                                            'bdmv_index': bdmv_index, 'chapter_index': j,
+                                                            'offset': get_time_str(shift)}
+                                sub_index += 1
+                    time_shift += (play_item_in_out_time[2] - play_item_in_out_time[1]) / 45000
+            else:
+                sub_index += 1
             if sub_index == len(subtitle_files):
                 break
 
@@ -824,37 +855,45 @@ class BluraySubtitleGUI(QWidget):
                     self.checkbox1.isChecked(),
                     None
                 ).generate_configuration(self.table1)
-                for subtitle_index in range(self.table2.rowCount()):
-                    con = configuration.get(subtitle_index)
-                    if con:
-                        self.table2.setItem(subtitle_index, 2, QTableWidgetItem(str(con['bdmv_index'])))
-                        chapter_combo = QComboBox()
-                        for bi in range(self.table1.rowCount()):
-                            if bi + 1 == con['bdmv_index']:
-                                info: QTableWidget = self.table1.cellWidget(bi, 2)
-                                for mi in range(info.rowCount()):
-                                    main_btn: QToolButton = info.cellWidget(mi, 3)
-                                    if main_btn.isChecked():
-                                        mpls_file = info.item(mi, 0).text()
-                                        root = self.table1.item(bi, 0).text()
-                                        select_mpls = os.path.join(root, 'BDMV', 'PLAYLIST', mpls_file)
-                                        rows = sum(map(len, Chapter(select_mpls).mark_info.values()))
-                                        chapter_combo.addItems([str(r + 1) for r in range(rows)])
-                                        chapter_combo.setCurrentIndex(con['chapter_index'] - 1)
-                                        chapter_combo.currentIndexChanged.connect(
-                                            partial(self.on_chapter_combo, subtitle_index, chapter_combo))
-                        self.table2.setCellWidget(subtitle_index, 3, chapter_combo)
-                        self.table2.setItem(subtitle_index, 4, QTableWidgetItem(con['offset']))
-                self.table2.resizeColumnsToContents()
+                self.on_configuration(configuration)
             except:
                 traceback.print_exc()
                 self.table2.clear()
                 self.table2.setColumnCount(len(SUBTITLE_LABELS))
                 self.table2.setHorizontalHeaderLabels(SUBTITLE_LABELS)
 
+    def on_configuration(self, configuration: Dict[int, Dict[str, Union[int, str]]]):
+        for subtitle_index in range(self.table2.rowCount()):
+            con = configuration.get(subtitle_index)
+            if con:
+                self.table2.setItem(subtitle_index, 2, QTableWidgetItem(str(con['bdmv_index'])))
+                chapter_combo = QComboBox()
+                for bi in range(self.table1.rowCount()):
+                    if bi + 1 == con['bdmv_index']:
+                        info: QTableWidget = self.table1.cellWidget(bi, 2)
+                        for mi in range(info.rowCount()):
+                            main_btn: QToolButton = info.cellWidget(mi, 3)
+                            if main_btn.isChecked():
+                                mpls_file = info.item(mi, 0).text()
+                                root = self.table1.item(bi, 0).text()
+                                select_mpls = os.path.join(root, 'BDMV', 'PLAYLIST', mpls_file)
+                                rows = sum(map(len, Chapter(select_mpls).mark_info.values()))
+                                chapter_combo.addItems([str(r + 1) for r in range(rows)])
+                                chapter_combo.setCurrentIndex(con['chapter_index'] - 1)
+                                chapter_combo.currentIndexChanged.connect(
+                                    partial(self.on_chapter_combo, subtitle_index, chapter_combo))
+                self.table2.setCellWidget(subtitle_index, 3, chapter_combo)
+                self.table2.setItem(subtitle_index, 4, QTableWidgetItem(con['offset']))
+        self.table2.resizeColumnsToContents()
+
     def on_chapter_combo(self, subtitle_index: int, chapter_combo: QComboBox):
-        print(subtitle_index)
-        print(chapter_combo.currentText())
+        configuration = BluraySubtitle(
+                        self.bdmv_folder_path.text(),
+                        self.subtitle_folder_path.text(),
+                        self.checkbox1.isChecked(),
+                        None
+                    ).generate_configuration(self.table1, subtitle_index, chapter_combo.currentIndex() + 1)
+        self.on_configuration(configuration)
 
     def on_button_play(self, mpls_path: str, btn: QToolButton):
         if btn.text() == 'preview':
